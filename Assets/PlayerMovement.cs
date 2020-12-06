5735c62dcd78166cb93a0d7c610844161dbe9726
szefuncio512 <74043484+szefuncio512@users.noreply.github.com>
2020-12-06 18:21:53 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Leftshift))
+        {
+            Run();
+        }
+
+               
+          
+            
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    public void Run()
+    {
+        animator.SetTrigger("Run");
+    }
+
+
+}
